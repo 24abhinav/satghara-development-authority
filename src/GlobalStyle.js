5c76019dce838285e657d46e2ae86e24d379bf71
Abhinav Anand <abhinav1813@gmail.com>
2024-07-06 11:08:37 +0530

    Update GlobalStyle.js

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -186,7 +186,7 @@ const GlobalStyle = styled.div`
 
    .cards {
         display: grid;
-        grid-template-columns: repeat( auto-fit, minmax(250px, 1fr) );
+        grid-template-columns: repeat( auto-fit, minmax(350px, 1fr) );
         grid-gap: 10px;
 
         .card {
